88a850e4db9e9f89d4e3eb6e85dd8647eb8fd745
jpare006 <jpare006@ucr.edu>
2020-03-07 23:02:02 -0800

    Library interface version 1 complete

--- test/adc/test_adc.c
+++ test/adc/test_adc.c
@@ -1,16 +1,32 @@
 #include "unity.h"
 
 #include "adc.h"
+#include "mock_atmega168_adc.h"
+
+uint8_t expectedData;
 
 void setUp(void)
 {
+	expectedData = 200;
 }
 
 void tearDown(void)
 {
 }
 
-void test_adc_NeedToImplement(void)
+void test_AdcInit(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement adc");
+    create_Expect();
+
+    adc_init();
+}
+
+void test_AdcRead(void)
+{
+	uint8_t actualData;
+
+	read_ExpectAndReturn(expectedData);
+
+	actualData = adc_read();
+	TEST_ASSERT_EQUAL_UINT8(expectedData, actualData);
 }
